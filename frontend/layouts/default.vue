0923683722bd79e58893c9753ad22059d5651387
Niklas.Buchfink <niklas.buchfink@web.de>
2021-04-30 18:48:44 +0200

    Chakra UI uninstalled

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,30 +1,11 @@
 <template>
-  <div class="container">
-    <CThemeProvider>
-      <CColorModeProvider>
-        <CBox font-family="body" as="main">
-          <CReset />
-          <Nuxt />
-        </CBox>
-      </CColorModeProvider>
-    </CThemeProvider>
+  <div>
+    <next />
   </div>
 </template>
-<script>
-import {
-  CThemeProvider,
-  CColorModeProvider,
-  CReset,
-  CBox,
-} from '@chakra-ui/vue'
 
-export default {
-  name: 'App',
-  components: {
-    CThemeProvider,
-    CColorModeProvider,
-    CReset,
-    CBox,
-  },
-}
+<script>
+export default {}
 </script>
+
+<style lang="scss" scoped></style>
