46b2138bcd217fa778bec0b49a441febb7b80410
Niklas.Buchfink <niklas.buchfink@web.de>
2021-05-21 14:53:18 +0200

    Implemenation Rastergrid

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -20,12 +20,18 @@ export default {
       { name: 'author', content: 'Niklas Buchfink' },
       { name: 'description', content: 'Portfolio | Niklas Buchfink' },
       { name: 'keywords', content: 'Design, Engineering, Portfolio' },
+      { name: 'robots', content: 'noindex'}
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/scss/main.scss'],
+  css: [
+    '~/assets/css/raster2.css',
+    '~/assets/scss/main.scss',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -75,5 +81,11 @@ export default {
   build: {
     extractCSS: true,
     extend(config, ctx) {},
+  },
+
+  vue: {
+    config: {
+      ignoredElements: ['r-grid', 'r-cell'],
+    }
   },
 }
